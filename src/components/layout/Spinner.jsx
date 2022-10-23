4c643ff88ad07bb0c3ffc4f1228721eba990e6e6
pooja <poojaparameshwarappa@gmail.com>
2022-10-23 19:34:20 +0530

    deploy my react app to github pages

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,17 @@
+import spinner from "./assets/spinner.gif";
+import React from "react";
+
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <img
+        width={180}
+        className="text-center mx-auto"
+        src={spinner}
+        alt="Loading...."
+      ></img>
+    </div>
+  );
+}
+
+export default Spinner;
